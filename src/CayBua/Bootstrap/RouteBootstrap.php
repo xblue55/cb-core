be6bd828a0d8f09ae72002d5d8f2abee15ba2774
Nguyen Ngoc Luong <nnluong.it@gmail.com>
2017-06-15 14:45:03 +0700

    (feat) remove document.html in core

--- src/CayBua/Bootstrap/RouteBootstrap.php
+++ src/CayBua/Bootstrap/RouteBootstrap.php
@@ -28,16 +28,5 @@ class RouteBootstrap implements BootstrapInterface
             $view->setVar('client', $config->clientHostName);
             return $view->render('general/proxy');
         });
-
-        $api->get('/documentation.html', function() use ($api, $config) {
-
-            /** @var \Phalcon\Mvc\View\Simple $view */
-            $view = $api->di->get(Services::VIEW);
-
-            $view->setVar('title', $config->application->title);
-            $view->setVar('description', $config->application->description);
-            $view->setVar('documentationPath', $config->hostName . '/export/documentation.json');
-            return $view->render('general/documentation');
-        });
     }
 }
