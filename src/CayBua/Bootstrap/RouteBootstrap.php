a7575960533484e1633d6556bf709f5a432fd059
Dinh Nhat Bang <dinhnhatbang@gmail.com>
2017-07-17 10:35:54 +0700

    Fix documentation view (Change View to SimpleView)

--- src/CayBua/Bootstrap/RouteBootstrap.php
+++ src/CayBua/Bootstrap/RouteBootstrap.php
@@ -13,7 +13,7 @@ use CayBua\BootstrapInterface;
 
 use Phalcon\Config;
 use Phalcon\DiInterface;
-use Phalcon\Mvc\View;
+use Phalcon\Mvc\View\Simple as SimpleView;
 
 class RouteBootstrap implements BootstrapInterface
 {
@@ -22,7 +22,7 @@ class RouteBootstrap implements BootstrapInterface
         $api->get('/' . $config->get('domainName') . '/documentation.html',
             function () use ($api, $config) {
                 /** @var \Phalcon\Mvc\View\Simple $view */
-                $view = new View;
+                $view = new SimpleView;
                 $view->setViewsDir(__DIR__ . '/../Views/');
                 $view->setVar('title', $config->get('application')->title);
                 $view->setVar('description', $config->get('application')->description);
@@ -32,7 +32,7 @@ class RouteBootstrap implements BootstrapInterface
                     '/' . $config->get('domainName') .
                     '/export/documentation.json'
                 );
-                return $view->render('general','documentation');
+                return $view->render('general/documentation');
             });
     }
 }
